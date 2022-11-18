fa1a1353a375b57414a0d4a1778be82baa2819cb
Rishabh Srivastava <rishabhchandra2408@gmail.com>
2022-11-18 17:10:15 +0530

    Ecommerce Project Structure Created

---
+++ Shop2Home/src/main/java/com/shop2home/Shop2HomeApplication.java
@@ -0,0 +1,13 @@
+package com.shop2home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop2HomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop2HomeApplication.class, args);
+	}
+
+}
